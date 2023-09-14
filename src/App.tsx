b2b05e13779ea2c952cf54e67b00eb8c6a130f0f
nsx_69 <mrmanafest16@gmail.com>
2023-09-15 00:36:55 +0500

    Changed all

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components';
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-        <Box>
-            <StyledBtn>
-                Hello World
-            </StyledBtn>
-        </Box>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Box>
+                <Title>Hello World</Title>
+                <GitHub href={"https://github.com/nrzklv"} target={"_blank"}>GitHub Account</GitHub>
+                <GitHubProject href={"https://github.com/nrzklv/IT-Kamasutra.git"} target={"_blank"}>GitHub Project</GitHubProject>
+                <CRA href={"https://create-react-app.dev/docs/adding-typescript/"} target={"_blank"}>Create React App</CRA>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
 
-const StyledBtn = styled.button `
-  border: 1px solid black;
-  border-radius: 5px;
-  padding: 10px 20px;
-  background-color: darkcyan;
-  font-size: 3rem;
-  font-weight: bold;
-`
-
 const Box = styled.div `
   display: flex;
-  height: 100vh;
-  justify-content: center;
   align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+  gap: 35px;
 `
+
+const Title = styled.h1 `
+  color: #fff;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: darkorange;
+  }
+`
+
+const GitHub = styled.a `
+  color: #fff;
+  text-decoration: none;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: darkorange;
+  }
+`
+
+const GitHubProject = styled(GitHub) `
+  color: #fff;
+`
+
+const CRA = styled(GitHubProject) `
+color: #fff;
+`
+
 
