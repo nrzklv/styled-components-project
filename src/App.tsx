7570ef34ab4a5080fe8a00a64506294bbf7a75f8
nsx_69 <mrmanafest16@gmail.com>
2023-09-14 16:54:14 +0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Box>
+            <StyledBtn>
+                Hello World
+            </StyledBtn>
+        </Box>
     </div>
   );
 }
 
 export default App;
+
+const StyledBtn = styled.button `
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px 20px;
+  background-color: darkcyan;
+  font-size: 3rem;
+  font-weight: bold;
+`
+
+const Box = styled.div `
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`
+
