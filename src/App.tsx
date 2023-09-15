fe63f34bb70736a613f664e6b16dc013d947ca41
nsx_69 <mrmanafest16@gmail.com>
2023-09-15 16:36:25 +0500

    Styles are divided into necessary parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,22 @@
 import React from 'react';
 import './App.css';
-import styled from "styled-components";
+import Box from "./components/Box.styled";
+import BtnComp from "./components/Button.styled";
+import GitHubAcc from "./components/Link.styled";
+import GitHubProject from "./components/GitHubProject.styled";
+import CreateReactApp from "./components/CreateReactApp.styled";
 
 function App() {
     return (
         <div className="App">
             <Box>
-                <Title>Hello World</Title>
-                <GitHub href={"https://github.com/nrzklv"} target={"_blank"}>GitHub Account</GitHub>
+                <BtnComp as="a" href={"#"}>Hello World</BtnComp>
+                <GitHubAcc href={"https://github.com/nrzklv"} target={"_blank"}>GitHub Account</GitHubAcc>
                 <GitHubProject href={"https://github.com/nrzklv/IT-Kamasutra.git"} target={"_blank"}>GitHub Project</GitHubProject>
-                <CRA href={"https://create-react-app.dev/docs/adding-typescript/"} target={"_blank"}>Create React App</CRA>
+                <CreateReactApp href={"https://create-react-app.dev/docs/adding-typescript/"} target={"_blank"}>Create React App</CreateReactApp>
             </Box>
         </div>
     );
 }
 
 export default App;
-
-const Box = styled.div `
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 20px;
-  gap: 35px;
-`
-
-const Title = styled.h1 `
-  color: #fff;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: darkorange;
-  }
-`
-
-const GitHub = styled.a `
-  color: #fff;
-  text-decoration: none;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: darkorange;
-  }
-`
-
-const GitHubProject = styled(GitHub) `
-  color: #fff;
-`
-
-const CRA = styled(GitHubProject) `
-color: #fff;
-`
-
-
---
+++ src/components/Box.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Box = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+  gap: 35px;
+`
+
+export default Box
